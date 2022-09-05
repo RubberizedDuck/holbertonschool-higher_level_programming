303d9335db322483e60a43b86ff7d8b52bdaa392
RubberizedDuck <dyl_2536@hotmail.com>
2022-09-04 19:07:02 -0700

    task 3

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-for n in range(97, 123):
-    if chr(n) == 'e' or chr(n) == 'q':
+for n in range(ord('a'), ord('z')+1):
+    if n == ord('e') or n == ord('q'):
         continue
     print(chr(n), end="")
