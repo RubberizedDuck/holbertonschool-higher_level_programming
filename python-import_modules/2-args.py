891e49706564fd50050f75b42a40ccc52b2bd451
RubberizedDuck <dyl_2536@hotmail.com>
2022-09-07 19:38:30 -0700

    Program that prints a number of arguments

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import sys
+
+if __name__ == "__main__":
+    argc = len(sys.argv) - 1
+    if argc == 0:
+        print("{} arguments.".format(argc))
+    elif argc == 1:
+        print("{} argument:".format(argc))
+    else:
+        print("{} arguments:".format(argc))
+
+        i = 0
+        for args in sys.argv:
+            if i != 0:
+                print("{}: {}".format(i, args))
+            i += 1
