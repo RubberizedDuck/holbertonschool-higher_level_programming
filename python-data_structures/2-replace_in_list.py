83afbcbf3c11cd93418adf5c47c3a3009574cd24
RubberizedDuck <dyl_2536@hotmail.com>
2022-09-11 17:43:03 -0700

    Replaces element at given index with new value

---
+++ python-data_structures/2-replace_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def replace_in_list(my_list, idx, element):
+    count = len(my_list)
+    if idx < 0 or idx >= count:
+        return None
+
+    my_list[idx] = element
+    return my_list
