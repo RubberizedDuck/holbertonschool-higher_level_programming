b4de8b2ba4f4f98638e5f8cf7dafedb31bb3fdb1
RubberizedDuck <dyl_2536@hotmail.com>
2022-09-11 18:04:19 -0700

    Prints new list followed by original list

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def new_in_list(my_list, idx, element):
+    new_list = my_list[:]
+    count = len(my_list)
+    if idx < 0 or idx >= count:
+        return my_list
+
+    new_list[idx] = element
+    return new_list
