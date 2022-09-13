5f8d6ebc117adacfda67e6df327fa89373499261
RubberizedDuck <dyl_2536@hotmail.com>
2022-09-12 21:33:08 -0700

    Fix: rework of code

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,12 +2,6 @@
 
 
 def best_score(a_dictionary):
-    if a_dictionary is None:
-        return None
-
-    highest_score = 0
-    for key, value in a_dictionary.items():
-        if value > highest_score:
-            highest_score = value
-            win = key
-    return win
+    if a_dictionary:
+        return max(a_dictionary, key=a_dictionary.get)
+    return None
