21968877d4a3ed779a64c8b602a7465c15cb2ff1
RubberizedDuck <dyl_2536@hotmail.com>
2022-09-12 21:28:07 -0700

    Returns highest value key

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def best_score(a_dictionary):
+    if a_dictionary == None:
+        return None
+
+    highest_score = 0
+    for key, value in a_dictionary.items():
+        if value > highest_score:
+            highest_score = value
+            win = key
+    return win
