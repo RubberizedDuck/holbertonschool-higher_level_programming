2b4f3563d36a588421fe12be82c6176d6dd5fc47
RubberizedDuck <dyl_2536@hotmail.com>
2022-09-12 18:17:35 -0700

    Indent code to be more readable

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,11 @@
 
 
 def print_matrix_integer(matrix=[[]]):
-    print('\n'.join([' '.join(['{:d}'.format(element) for element in row])
-                     for row in matrix]))
+    print(
+        '\n'.join(
+            [' '.join(
+                ['{:d}'.format(element) for element in row]
+            )
+                     for row in matrix]
+        )
+    )
