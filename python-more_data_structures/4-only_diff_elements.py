79504859f51879ab9c8eb15716f8797f0668931d
RubberizedDuck <dyl_2536@hotmail.com>
2022-09-12 19:45:41 -0700

    Fix: line break

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -6,8 +6,8 @@ def only_diff_elements(set_1, set_2):
         element for element in (
             set_1 | set_2
         )
-        if element in set_1 and element not in set_2
-        or element in set_2 and element not in set_1
+        if element in set_1 and element not in set_2 or
+        element in set_2 and element not in set_1
     ]
 
     return result
