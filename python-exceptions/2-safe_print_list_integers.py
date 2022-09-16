116556eef7f8ea59b58715c93d3eff24c01ccaec
RubberizedDuck <dyl_2536@hotmail.com>
2022-09-15 18:56:59 -0700

    Fix: Add result var and return it

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
+safe_print_list_integers = \
+    __import__('2-safe_print_list_integers').safe_print_list_integers
 
-
-def safe_print_list_integers(my_list=[], x=0):
-    for i in range(x):
-        try:
-            print("{:d}".format(my_list[i]), end="")
-        except (ValueError, TypeError):
-            continue
-    print()
-    return x
+my_list = ["Holberton", 1, 2, "H", 3, 4, [1, 2, 3,4]]
+x = len(my_list)
+nb_print = safe_print_list_integers(my_list, x)
+print("{:d}".format(nb_print))
