ccc04cb2bc2bc76c2e5a1e6a0a5c8c681880a716
RubberizedDuck <dyl_2536@hotmail.com>
2022-09-15 19:04:45 -0700

    Fix: revert file and add var

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
-safe_print_list_integers = \
-    __import__('2-safe_print_list_integers').safe_print_list_integers
 
-my_list = ["Holberton", 1, 2, "H", 3, 4, [1, 2, 3,4]]
-x = len(my_list)
-nb_print = safe_print_list_integers(my_list, x)
-print("{:d}".format(nb_print))
+
+def safe_print_list_integers(my_list=[], x=0):
+    result = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            result += 1
+        except (ValueError, TypeError):
+            continue
+    print()
+    return result
