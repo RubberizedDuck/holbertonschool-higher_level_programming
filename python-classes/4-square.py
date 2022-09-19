c314e109d6e0884be51560e71048f4e1b2ee577b
RubberizedDuck <dyl_2536@hotmail.com>
2022-09-18 18:45:44 -0700

    Adding task 4

---
+++ python-classes/4-square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""defines class Square"""
+
+
+class Square:
+    """class for Square"""
+    def __init__(self, size=0):
+        """
+        runs a check to ensure that size is integer value
+        and that it is not less than 0
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """
+        returns the area size of the square
+        """
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """
+        Returns the privately stored variable size
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        sets the private variable of size &
+        checks that size is the correct type and value
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
