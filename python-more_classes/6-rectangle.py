86d4f582a92b52d0d7c9167fcc4ced0a21d88a39
RubberizedDuck <dyl_2536@hotmail.com>
2022-09-19 17:58:56 -0700

    Fix: doc for class

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -3,8 +3,9 @@
 
 
 class Rectangle:
-    number_of_instances = 0
     """ rectangle class """
+    number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         """
         runs a check to ensure that width & height are
