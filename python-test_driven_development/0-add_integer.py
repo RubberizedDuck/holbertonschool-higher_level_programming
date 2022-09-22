ffd59f01bc39456c5a017e6cd14bde4e61e44e9d
RubberizedDuck <dyl_2536@hotmail.com>
2022-09-21 17:36:51 -0700

    Function to add return sum of 2 values as ints

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+checks if a & b are type of int or float
+if a or b equals a float, converts them to an int
+then returns value of a + b
+"""
+def add_integer(a, b=98):
+    """
+    returns the value of a + b
+    """
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
+    if isinstance(a, float):
+        a = int(a)
+    if isinstance(b, float):
+        b = int(b)
+    return a + b
