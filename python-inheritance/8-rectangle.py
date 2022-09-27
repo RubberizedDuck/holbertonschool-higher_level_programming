c6aec9aff7d0f00aa363999f41b6c2f1fd4c5512
RubberizedDuck <dyl_2536@hotmail.com>
2022-09-26 21:08:49 -0700

    Fix: remove BG and import instead

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,28 +1,14 @@
 #!/usr/bin/python3
-""" This module raises exceptions """
+""" This module inherits BaseGeometry class """
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
-
-class BaseGeometry:
-    """ Class to raise exceptions """
-    def area(self):
-        """ Exception raised for area """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        Raises exceptions if value is not int
-        or is less than or equal to 0
-        """
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
 
 class Rectangle(BaseGeometry):
     """ Rectangle is subclass of BaseGeometry """
+
     def __init__(self, width, height):
         """ init for Rectangle, using inherited integer_validator """
-        self.__width = width
-        self.__height = height
         self.integer_validator("width", width)
         self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
