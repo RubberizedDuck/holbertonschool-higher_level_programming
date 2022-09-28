b75868dbc3900dd220258df02d8e7e89a5963e39
RubberizedDuck <dyl_2536@hotmail.com>
2022-09-28 16:56:15 -0700

    Adding task 11

---
+++ python-input_output/11-student.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+This module contains 1 class
+Studen
+"""
+
+
+class Student:
+
+    def __init__(self, first_name, last_name, age):
+        """ instantiation for class """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        returns dictionary representation of the class
+        """
+        if type(attrs) is list:
+            for var in attrs:
+                if type(var) != str:
+                    return self.__dict__
+            return {var: self.__dict__[var] for var in attrs
+                    if hasattr(self, var)}
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        """
+        replaces all attributes of Student instance
+        """
+        for key in json:
+            setattr(self, key, json[key])
