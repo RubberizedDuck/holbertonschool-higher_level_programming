32870df0db4148df27e2268617011c4ac2d3c4bf
RubberizedDuck <dyl_2536@hotmail.com>
2022-09-28 15:36:26 -0700

    Fix: remove import

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -3,11 +3,10 @@
 This module is designed to convert a class
 to a JSON dict
 """
-import json
 
 
 def class_to_json(obj):
     """
     Functions returns json dictonary of a class object
     """
-    return json.dumps(obj.__dict__)
+    return obj.__dict__
