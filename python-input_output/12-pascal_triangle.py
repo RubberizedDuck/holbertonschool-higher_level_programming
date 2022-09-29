33fc98f757182154e2c233198728fb53da7ff0bb
RubberizedDuck <dyl_2536@hotmail.com>
2022-09-28 20:18:32 -0700

    Pascal triangle challenge

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+
+
+
+def pascal_triangle(n):
+    pascal_list = []
+    if n <= 0:
+        return pascal_list
+
+    for row in range(n):
+        if row == 0 or row == 1:
+            pascal_list.append([1 for c in range(0, row + 1)])
+        else:
+            line = []
+            for column in range(0, len(pascal_list[row - 1]) + 1):
+                if column == 0 or column == len(pascal_list[row - 1]):
+                    line.append(1)
+                else:
+                    line.append(pascal_list[row - 1][column - 1] +
+                                pascal_list[row - 1][column])
+            pascal_list.append(line)
+
+    return pascal_list
