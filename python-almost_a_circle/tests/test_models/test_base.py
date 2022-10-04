597187c75a70c6433de77180f26f9769c1f2ec4c
RubberizedDuck <dyl_2536@hotmail.com>
2022-10-03 20:28:03 -0700

    Change import method for base

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -4,9 +4,11 @@
 
 import unittest
 import pycodestyle
+import json
 from models.rectangle import Rectangle
 from models.square import Square
-from models.base import Base
+from models import base
+Base = base.Base
 
 
 class TestBaseDocs(unittest.TestCase):
@@ -30,7 +32,7 @@ class TestBaseDocs(unittest.TestCase):
         """ Checks pycodestyle for test_base """
         style = pycodestyle.StyleGuide(quiet=True)
         result = style.check_files(['tests/test_models/test_base.py'])
-        self.assertEqual(result.total_errors, 1,
+        self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
 
 
@@ -54,3 +56,23 @@ class TestBase(unittest.TestCase):
         """ calling class with more than 1 arg """
         with self.assertRaises(TypeError):
             base3 = Base(2, 5)
+
+    def test_empty_to_json_sting(self):
+        """ Test passing empty list/None """
+        j_string = Base.to_j_string([])
+        self.assertTrue(type(j_string) is str)
+        self.assertEqual(j_string, "[]")
+
+    def test_empty_from_json_sting(self):
+        """ Test passing empty string """
+        self.assertEqual([], Base.from_j_string(""))
+
+    def test_None_to_json_sting(self):
+        """ Test passing None to JSON """
+        j_string = Base.to_j_string(None)
+        self.assertTrue(type(j_string) is str)
+        self.assertEqual(j_string, "[]")
+
+    def test_None_from_json_sting(self):
+        """ Test for passing None from JSON """
+        self.assertEqual([], Base.from_j_string(None))
