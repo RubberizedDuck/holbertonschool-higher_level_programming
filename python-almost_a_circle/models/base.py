8ec23894f477264b34188fa522075f45b03aa09b
RubberizedDuck <dyl_2536@hotmail.com>
2022-10-03 21:57:16 -0700

    Fix: spelling error

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -35,7 +35,7 @@ class Base:
     @classmethod
     def save_to_file(cls, list_objs):
         """ writes the JSON string rep of list_objs to a file """
-        filename = cls.__name__ + "json"
+        filename = cls.__name__ + ".json"
         file_list = []
         if list_objs is not None:
             for item in list_objs:
