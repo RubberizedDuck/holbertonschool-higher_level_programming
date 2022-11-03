2c065785b9b714d8a6efe17bd0b0b99d3db8b969
RubberizedDuck <dyl_2536@hotmail.com>
2022-11-02 18:24:58 -0700

    Creates a second_table

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,12 @@
+-- Task 9: Full creation
+-- Script to create table 'second_table' and add multiple rows of data
+CREATE TABLE second_table (
+       id int,
+       name VARCHAR(256),
+       score int
+);
+INSERT INTO second_table (id, name, score)
+VALUES (1, 'John', 10),
+       (2, 'Alex', 3),
+       (3, 'Bob', 14),
+       (4, 'George', 8);
