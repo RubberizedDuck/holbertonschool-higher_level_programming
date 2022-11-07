2807fff2da1616c74a5bcd07fb1a3787ab6c6258
RubberizedDuck <dyl_2536@hotmail.com>
2022-11-06 16:06:48 -0800

    Fix: pycodestyle indent

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -8,7 +8,7 @@ import sys
 
 if __name__ == "__main__":
     dbConnect = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3])
+                                passwd=sys.argv[2], db=sys.argv[3])
     cur = dbConnect.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     query_results = cur.fetchall()
