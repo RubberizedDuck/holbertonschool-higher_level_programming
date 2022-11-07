e4db47a9ca3aeb71872d65674474d0325870555d
RubberizedDuck <dyl_2536@hotmail.com>
2022-11-06 16:05:22 -0800

    Add Task 0

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This module is a basic interaction with MySQLdb
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    dbConnect = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cur = dbConnect.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_results = cur.fetchall()
+    for row in query_results:
+        print(row)
