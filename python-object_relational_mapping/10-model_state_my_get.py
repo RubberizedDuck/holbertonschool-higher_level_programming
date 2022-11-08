e93318273e203b3549167f22f9043a75e5da62c1
RubberizedDuck <dyl_2536@hotmail.com>
2022-11-07 19:04:55 -0800

    Returns the id from user input State

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+This module is designed to return the state id
+from what the user inputs in argv[4]
+"""
+
+from sys import argv
+from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy import create_engine
+from model_state import Base, State
+
+if __name__ == "__main__":
+    user_input = argv[4]
+    engine = create_engine(
+        f'mysql+mysqldb://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}',
+        pool_pre_ping=True
+    )
+    Session = sessionmaker()
+
+    Session.configure(bind=engine)
+    session = Session()
+    state = session.query(State).filter(State.name.like(user_input)).first()
+    if not state:
+        print('Not Found')
+    else:
+        print(f'{state.id}')
