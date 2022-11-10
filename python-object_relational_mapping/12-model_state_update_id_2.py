553366ef37e00c67d659374c93e6ebfec7f772e7
RubberizedDuck <dyl_2536@hotmail.com>
2022-11-10 14:27:27 -0800

    Updates State id 2 name

---
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This module is designed to update the State name with id '2'
+to 'New Mexico'
+"""
+
+from sys import argv
+from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sqlalchemy import update
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+        f'mysql+mysqldb://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}',
+        pool_pre_ping=True
+    )
+    Session = sessionmaker(bind=engine)
+    with Session.begin() as session:
+        update_state = session.query(State).filter(State.id==2).first()
+        update_state.name = 'New Mexico'
+        session.commit()
