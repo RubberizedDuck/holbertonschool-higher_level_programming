89e913a0dec9703729a4e4abe8887efd686e4b6e
RubberizedDuck <dyl_2536@hotmail.com>
2022-12-05 17:47:13 +1100

    Fix: added temp of width

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -23,8 +23,9 @@ class Rectangle {
   }
 
   rotate () {
+    const temp = this.width;
     this.width = this.height;
-    this.height = this.width;
+    this.height = temp;
   }
 }
 module.exports = Rectangle;
