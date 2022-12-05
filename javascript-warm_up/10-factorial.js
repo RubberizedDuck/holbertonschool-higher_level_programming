db34256afce856d6a5afcb6cbf1b194c3fe45cd9
RubberizedDuck <dyl_2536@hotmail.com>
2022-12-05 14:12:29 +1100

    Fix: semistandard

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -3,13 +3,13 @@
 let num = Number(process.argv[2]);
 
 if (!num) {
-	num = 1;
+  num = 1;
 }
 function factorial (num) {
-	if (num === 0 || num === 1) {
-		return 1;
-	} else {
-		return num * factorial(num - 1);
-	}
+  if (num === 0 || num === 1) {
+    return 1;
+  } else {
+    return num * factorial(num - 1);
+  }
 }
 console.log(factorial(num));
