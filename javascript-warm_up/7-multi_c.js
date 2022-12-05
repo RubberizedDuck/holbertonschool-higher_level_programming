c9ad157205053d7a1286fa66b561ade52d2d89c3
RubberizedDuck <dyl_2536@hotmail.com>
2022-12-04 17:23:56 -0800

    Push for vscode

---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const myNum = Number(process.argv[2]);
+if (foo bar) {
+  
+}
+if (myNum) {
+  let x = 0;
+  while (x !== myNum) {
+    if (myNum < 0) {
+      break
+    } else {
+      console.log('C is fun');
+      x++;
+    }
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
