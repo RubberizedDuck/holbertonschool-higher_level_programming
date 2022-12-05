d5852d05f4e597c123dd257c03f2d415a50b05d7
RubberizedDuck <dyl_2536@hotmail.com>
2022-12-04 19:34:07 -0800

    Fix: semicolon inside function

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -5,6 +5,6 @@ if (arr.length < 2) {
   console.log('0');
 } else {
 // sorts and flips the array to be in descending order
-  arr.sort(function (a, b) { return b - a });
+  arr.sort(function (a, b) { return b - a; });
   console.log(arr[1]);
 }
