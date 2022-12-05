c5ea167e03feb45cd31403f9ccc0d451fbb240b0
RubberizedDuck <dyl_2536@hotmail.com>
2022-12-05 14:29:53 +1100

    Fix: move comments

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -4,6 +4,7 @@ const arr = process.argv.splice(2);
 if (arr.length < 2) {
   console.log('0');
 } else {
-  arr.sort(function (a, b) { return b - a }); // sorts and flips the array to be in descending order;
+	// sorts and flips the array to be in descending order
+  arr.sort(function (a, b) { return b - a });
   console.log(arr[1]);
 }
