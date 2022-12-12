0c5366fab90856dd74f5f3cd17790b966ce1d36c
RubberizedDuck <dyl_2536@hotmail.com>
2022-12-12 11:36:39 +1100

    Returns star wars title

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const axios = require('axios');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+axios.get(url).then(function (response) {
+  console.log(response.data.title);
+});
